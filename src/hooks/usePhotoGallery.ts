53113480b7a43bafc27e830f73bd28a75273f082
s-sandra <aleksandra.shtabnaya@gmail.com>
2020-03-26 20:46:00 -0400

    Loaded photos from filesystem.

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -5,11 +5,14 @@ import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
 
+const PHOTO_STORAGE = "photos";
+
 export function usePhotoGallery() {
 
     const { getPhoto } = useCamera();
     const [photos, setPhotos] = useState<Photo[]>([]);
     const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+    const { get, set } = useStorage();
 
     const savePicture = async (photo: CameraPhoto, fileName: string) => {
         const base64Data = await base64FromPath(photo.webPath!);
@@ -27,6 +30,22 @@ export function usePhotoGallery() {
           webviewPath: cameraPhoto.webPath
         };
     };
+
+    useEffect(() => {
+        const loadSaved = async () => {
+          const photosString = await get('photos');
+          const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+          for (let photo of photos) {
+            const file = await readFile({
+              path: photo.filepath,
+              directory: FilesystemDirectory.Data
+            });
+            photo.base64 = `data:image/jpeg;base64,${file.data}`;
+          }
+          setPhotos(photos);
+        };
+        loadSaved();
+    }, [get, readFile]);
   
     const takePhoto = async () => {
         const cameraPhoto = await getPhoto({
@@ -39,6 +58,14 @@ export function usePhotoGallery() {
         const savedFileImage = await savePicture(cameraPhoto, fileName);
         const newPhotos = [savedFileImage, ...photos];
         setPhotos(newPhotos);
+
+        set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
+            // Don't save the base64 representation of the photo data, 
+            // since it's already saved on the Filesystem
+            const photoCopy = { ...p };
+            delete photoCopy.base64;
+            return photoCopy;
+        })));
     };
   
     return {
